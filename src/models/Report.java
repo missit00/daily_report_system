0a3ff1e31d1b9a9333d56c191c8d483e7439917e
taro.kirameki <sen12589@yahoo.co.jp>
2020-03-22 15:02:41 +0900

    Modify top Page

--- src/models/Report.java
+++ src/models/Report.java
@@ -15,105 +15,92 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-@Table(name="reports")
-@NamedQueries({
-	@NamedQuery(
-		name = "getAllReports",
-	            query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
-	),
-	@NamedQuery(
-		name = "getReportsCount",
-	            query = "SELECT COUNT(r) FROM Report AS r"
-	),
-})
+@Table(name = "reports")
+@NamedQueries({ @NamedQuery(name = "getAllReports", query = "SELECT r FROM Report AS r ORDER BY r.id DESC"),
+		@NamedQuery(name = "getReportsCount", query = "SELECT COUNT(r) FROM Report AS r"),
+		@NamedQuery(name = "getMyAllReports", query = "SELECT r FROM Report AS r WHERE r.employee = :employee ORDER BY r.id DESC"),
+		@NamedQuery(name = "getMyReportsCount", query = "SELECT COUNT(r) FROM Report AS r WHERE r.employee = :employee") })
 @Entity
 public class Report {
-@Id
-@Column(name = "Id")
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Integer id;
+	@Id
+	@Column(name = "Id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-@ManyToOne
-@JoinColumn(name = "employee_id",nullable = false)
-private Employee employee;
+	@ManyToOne
+	@JoinColumn(name = "employee_id", nullable = false)
+	private Employee employee;
 
-public Integer getId() {
-	return id;
-}
+	public Integer getId() {
+		return id;
+	}
 
-public void setId(Integer id) {
-	this.id = id;
-}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-public Employee getEmployee() {
-	return employee;
-}
+	public Employee getEmployee() {
+		return employee;
+	}
 
-public void setEmployee(Employee employee) {
-	this.employee = employee;
-}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
 
-public Date getReport_date() {
-	return report_date;
-}
+	public Date getReport_date() {
+		return report_date;
+	}
 
-public void setReport_date(Date report_date) {
-	this.report_date = report_date;
-}
+	public void setReport_date(Date report_date) {
+		this.report_date = report_date;
+	}
 
-public String getTitle() {
-	return title;
-}
+	public String getTitle() {
+		return title;
+	}
 
-public void setTitle(String title) {
-	this.title = title;
-}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-public String getContent() {
-	return content;
-}
+	public String getContent() {
+		return content;
+	}
 
-public void setContent(String content) {
-	this.content = content;
-}
+	public void setContent(String content) {
+		this.content = content;
+	}
 
-public Timestamp getCreated_at() {
-	return created_at;
-}
+	public Timestamp getCreated_at() {
+		return created_at;
+	}
 
-public void setCreated_at(Timestamp created_at) {
-	this.created_at = created_at;
-}
+	public void setCreated_at(Timestamp created_at) {
+		this.created_at = created_at;
+	}
 
-public Timestamp getUpdate_at() {
-	return update_at;
-}
+	public Timestamp getUpdate_at() {
+		return update_at;
+	}
 
-public void setUpdate_at(Timestamp update_at) {
-	this.update_at = update_at;
-}
+	public void setUpdate_at(Timestamp update_at) {
+		this.update_at = update_at;
+	}
 
-@Column(name = "report_date" , nullable= false)
-private Date report_date;
+	@Column(name = "report_date", nullable = false)
+	private Date report_date;
 
-@Column(name = "title",length = 225,nullable = false)
-private String title;
+	@Column(name = "title", length = 225, nullable = false)
+	private String title;
 
-@Lob
-@Column(name = "content",nullable = false)
-private String content;
+	@Lob
+	@Column(name = "content", nullable = false)
+	private String content;
 
-@Column(name = "created_at",nullable = false)
-private Timestamp created_at;
+	@Column(name = "created_at", nullable = false)
+	private Timestamp created_at;
 
-@Column(name = "updated_at",nullable=false)
-private Timestamp update_at;
-
-
-
-
-
+	@Column(name = "updated_at", nullable = false)
+	private Timestamp update_at;
 
 }
-
-
